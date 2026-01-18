fb146ea04f3c85484e8cd3aba5d4447e74e5bb2d
Ahmad Muh'd-Danrangi <159925950+Danrangi@users.noreply.github.com>
2026-01-18 20:57:28 +0000

    commit

--- src/app/controllers/main.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, redirect, url_for
-
-bp = Blueprint('main', __name__)
-
-@bp.route('/')
-def index():
-    # When a user visits the root domain, redirect them to login
-    return redirect(url_for('auth.login'))
